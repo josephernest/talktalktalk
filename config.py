e312fc2affcc6ab6c5d110836e7470bdc1aa438f
josephernest <nouvellecollection@gmail.com>
2016-11-21 21:19:34 +0100

    config file + encoding modifications

---
+++ config.py
@@ -0,0 +1,4 @@
+PORT = 9000                    # if you change this line, change the port as well in .htaccess
+HOST = "127.0.0.1"
+ADMINNAME = 'admin'            # this username will be available if *and only if* the following username is entered in the input field:
+ADMINHIDDENNAME = 'adminxyz'
--- talktalktalk.py
+++ talktalktalk.py
@@ -10,16 +10,13 @@
 # license: MIT license
 
 
-PORT = 9000
-HOST = "127.0.0.1"
-ADMINNAME = 'admin'        # this username will be available if *and only if* the following username is entered in the input field:
-ADMINHIDDENNAME = 'adminxyz'
-
 import sys, time, json, bleach, time, threading, dumbdbm, random, re
 import daemon
 from bottle import route, run, view, request, post, ServerAdapter, get, static_file
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
+from geventwebsocket.exceptions import WebSocketError
+from config import PORT, HOST, ADMINNAME, ADMINHIDDENNAME
 
 idx = 0
 
@@ -43,13 +40,14 @@ def main():
 
     def send_userlist():
         for u in users.keys():
-            u.send(json.dumps({'type' : 'userlist', 'connected': users.values()}))
+            if not u.closed:
+                u.send(json.dumps({'type' : 'userlist', 'connected': users.values()}))
 
     def clean_username(usr, ws):
-        username = bleach.clean(usr).encode('utf8')     
-        username = re.sub('[‍ :]', '', username)      # removes " ", ":", and this evil char http://unicode-table.com/fr/200D/
+        username = bleach.clean(usr)
+        username = re.sub('[‍ :]', '', username)      # removes " ", ":", and the evil char "‍" http://unicode-table.com/fr/200D/
         if username.lower() == ADMINNAME or username == '':
-            username = 'user' + str(random.randint(0,1000))
+            username = 'user' + str(random.randint(0, 1000))
             ws.send(json.dumps({'type' : 'usernameunavailable', 'username' : username}))
         elif username.lower() == ADMINHIDDENNAME:
             username = ADMINNAME
@@ -77,48 +75,55 @@ def main():
     def chat(ws):
         global idx
         while True:
-            receivedmsg = ws.receive()
-            if receivedmsg is not None:
-                if len(receivedmsg) > 16384:      # this user is probably a spammer
-                    break
-                pings[ws] = time.time()
-                if receivedmsg == 'ping':         # ping/pong packet to make sure connection is still alive
-                    ws.send('id' + str(idx-1))    # send the latest message id in return
-                    if ws not in users:           # was deleted by dbworker
-                        ws.send(json.dumps({'type' : 'username'}))
-                else:
-                    msg = json.loads(receivedmsg)
-                    if msg['type'] == 'message':
-                        message = (bleach.clean(msg['message'])).strip().encode('utf8')
+            try:
+                receivedmsg = ws.receive()
+                if receivedmsg is not None:
+                    receivedmsg = receivedmsg.decode('utf8')
+                    if len(receivedmsg) > 4096:      # this user is probably a spammer
+                        break
+                    pings[ws] = time.time()
+                    if receivedmsg == 'ping':         # ping/pong packet to make sure connection is still alive
+                        ws.send('id' + str(idx-1))    # send the latest message id in return
+                        if ws not in users:           # was deleted by dbworker
+                            ws.send(json.dumps({'type' : 'username'}))
+                    else:
+                        msg = json.loads(receivedmsg)
+                        if msg['type'] == 'message':
+                            message = (bleach.clean(msg['message'])).strip()
 
-                        if ws not in users:         # is this really mandatory ?
-                            username = clean_username(msg['username'], ws)       
+                            if ws not in users:         # is this really mandatory ?
+                                username = clean_username(msg['username'], ws)       
+                                users[ws] = username
+                                send_userlist()
+
+                            if message:
+                                if len(message) > 1000:
+                                    message = message[:1000] + '...'
+                                s = json.dumps({'type' : 'message', 'message': message, 'username': users[ws], 'id': idx, 'datetime': int(time.time())})
+                                db[str(idx)] = s                # Neither dumbdbm nor shelve module allow integer as key... I'm still looking for a better solution!
+                                idx += 1
+                                for u in users.keys():
+                                    u.send(s)
+
+                        elif msg['type'] == 'messagesbefore':
+                            idbefore = msg['id']
+                            ws.send(json.dumps({'type' : 'messages', 'before': 1, 'messages': [db[str(i)] for i in range(max(0,idbefore - 100),idbefore)]}))
+
+                        elif msg['type'] == 'messagesafter':
+                            idafter = msg['id']
+                            ws.send(json.dumps({'type' : 'messages', 'before': 0, 'messages': [db[str(i)] for i in range(idafter,idx)]}))
+
+                        elif msg['type'] == 'username':
+                            username = clean_username(msg['username'], ws)
+                            if ws not in users:          # welcome new user
+                                ws.send(json.dumps({'type' : 'messages', 'before': 0, 'messages': [db[str(i)] for i in range(max(0,idx - 100),idx)]}))
                             users[ws] = username
                             send_userlist()
-
-                        if message:
-                            s = json.dumps({'type' : 'message', 'message': message, 'username': users[ws], 'id': idx, 'datetime': int(time.time())})
-                            db[str(idx)] = s                # Neither dumbdbm nor shelve module allow integer as key... I'm still looking for a better solution!
-                            idx += 1
-                            for u in users.keys():
-                                u.send(s)
-
-                    elif msg['type'] == 'messagesbefore':
-                        idbefore = msg['id']
-                        ws.send(json.dumps({'type' : 'messages', 'before': 1, 'messages': [db[str(i)] for i in range(max(0,idbefore - 100),idbefore)]}))
-
-                    elif msg['type'] == 'messagesafter':
-                        idafter = msg['id']
-                        ws.send(json.dumps({'type' : 'messages', 'before': 0, 'messages': [db[str(i)] for i in range(idafter,idx)]}))
-
-                    elif msg['type'] == 'username':
-                        username = clean_username(msg['username'], ws)
-                        if ws not in users:          # welcome new user
-                            ws.send(json.dumps({'type' : 'messages', 'before': 0, 'messages': [db[str(i)] for i in range(max(0,idx - 100),idx)]}))
-                        users[ws] = username
-                        send_userlist()
-            else:
+                else:
+                    break
+            except (WebSocketError, ValueError, UnicodeDecodeError):      # ValueError happens for example when "No JSON object could be decoded", would be interesting to log it
                 break
+
         if ws in users:
             del users[ws]
             del pings[ws]
@@ -146,10 +151,11 @@ if len(sys.argv) == 1:           # command line interactive mode
 
 elif len(sys.argv) == 2:         # daemon mode
     daemon = talktalktalk(pidfile='_.pid', stdout='log.txt', stderr='log.txt')
-    
+   
     if 'start' == sys.argv[1]: 
         daemon.start()
     elif 'stop' == sys.argv[1]: 
         daemon.stop()
     elif 'restart' == sys.argv[1]: 
         daemon.restart()
+        
