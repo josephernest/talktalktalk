f463a9c08098199f0f68a5214f7182c990fc0205
root <root@gget.it>
2016-11-22 20:57:17 +0100

    minor

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ PORT = 9000                    # if you change this line, change the port as wel
 HOST = "127.0.0.1"
 ADMINNAME = 'admin'            # this username will be available if *and only if* the following username is entered in the input field:
 ADMINHIDDENNAME = 'adminxyz'
-ALLOWEDTAGS = []               # tags allowed in message, could be ['a', 'b', 'em', 'code'], etc.
+ALLOWEDTAGS = []               # tags allowed in messages, could be ['a', 'b', 'em', 'code'], etc.
